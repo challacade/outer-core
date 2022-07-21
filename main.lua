b199b8a48cd59a19fc6e6b1d2392afda4a1e6baf
kyleschaub <kylejschaub@gmail.com>
2022-07-20 21:03:40 -0500

    Remove debug button presses

--- main.lua
+++ main.lua
@@ -34,42 +34,6 @@ function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
-
-    if key == 'u' then
-        spawnUnit(1, math.random(-5, 5), math.random(-3, 3))
-    end
-
-    if key == 'i' then
-        spawnUnit(2, math.random(-5, 5), math.random(-3, 3))
-    end
-
-    if key == 'o' then
-        spawnUnit(3, math.random(-5, 5), math.random(-3, 3))
-    end
-
-    if key == 'e' then
-        curtain:call()
-    end
-
-    if key == '1' and gamestate == 0 then
-        manager:startLevel(1)
-    end
-
-    if key == '2' and gamestate == 0 then
-        manager:startLevel(2)
-    end
-
-    if key == '3' and gamestate == 0 then
-        manager:startLevel(3)
-    end
-
-    if key == '4' and gamestate == 0 then
-        manager:startLevel(4)
-    end
-
-    if key == '5' and gamestate == 0 then
-        manager:startLevel(5)
-    end
 end
 
 function love.mousepressed(x, y, button)
